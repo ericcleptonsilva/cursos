aab5e02307c70a25ae0ca3c7790294d342d7f623
ericcleptonsilva <ericcleptonsilva@gmail.com>
2021-08-20 00:54:05 -0300

    Criando Class criada com sucesso

---
+++ Curso POO Java/Aula02/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.tampada = false;
+        c1.ponta = 0.5f;
+        c1.status();
+    }
+}
---
+++ Curso POO Java/Aula02/src/Caneta.java
@@ -0,0 +1,26 @@
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int cargar;
+    boolean tampada;
+
+    void status() {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta: " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.cargar);
+        System.out.println("Está tampada? " + this.tampada);
+    }
+
+    void rabiscar() {
+    }
+
+    void tampar() {
+
+    }
+
+    void destampar() {
+    }
+
+}
