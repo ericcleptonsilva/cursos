0258b77973dfc8f336591bc7de756d676c47419a
ericcleptonsilva <ericcleptonsilva@gmail.com>
2021-08-20 01:12:04 -0300

    Aula02 finalizado

--- Curso POO Java/Aula02/src/App.java
+++ Curso POO Java/Aula02/src/App.java
@@ -2,8 +2,18 @@ public class App {
     public static void main(String[] args) {
         Caneta c1 = new Caneta();
         c1.cor = "Azul";
-        c1.tampada = false;
         c1.ponta = 0.5f;
+        c1.destampar();
         c1.status();
+        c1.rabiscar();
+        System.out.println("-----------------------------------");
+        Caneta c2 = new Caneta();
+        c2.modelo = "Bic";
+        c2.cor = "Vermelho";
+        c2.ponta = 1.0f;
+        c2.tampar();
+        c2.status();
+        c2.rabiscar();
+
     }
 }
--- Curso POO Java/Aula02/src/Caneta.java
+++ Curso POO Java/Aula02/src/Caneta.java
@@ -7,20 +7,27 @@ public class Caneta {
 
     void status() {
         System.out.println("Modelo: " + this.modelo);
+        System.out.println("Está tampada? " + this.tampada);
         System.out.println("Uma caneta: " + this.cor);
         System.out.println("Ponta: " + this.ponta);
         System.out.println("Carga: " + this.cargar);
-        System.out.println("Está tampada? " + this.tampada);
     }
 
     void rabiscar() {
+        if (this.tampada == true) {
+            System.out.println("ERRO! Não posso rabiscar!");
+        } else {
+            System.out.println("Estou Rabiscando!");
+        }
     }
 
     void tampar() {
+        this.tampada = true;
 
     }
 
     void destampar() {
+        this.tampada = false;
     }
 
 }
