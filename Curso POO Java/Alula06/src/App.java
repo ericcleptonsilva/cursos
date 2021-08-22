7456705792e0a9e3728b695ffb2c94233381b490
ericcleptonsilva <ericcleptonsilva@gmail.com>
2021-08-22 20:06:00 -0300

    Aula06

---
+++ Curso POO Java/Alula06/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.play();
+        c.abrirMenu();
+    }
+}
