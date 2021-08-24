4014f641ee6b806d8f418a1ac7f01e4c701fe8ba
ericcleptonsilva <ericcleptonsilva@gmail.com>
2021-08-24 15:23:03 -0300

    Alula07

---
+++ Curso POO Java/Aula07/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        Lutador l = new Lutador("Eric", "Brasileiro", 37, 170.0f, 100.0f, 1, 1, 2);
+        l.apresentar();
+        l.status();
+    }
+}
