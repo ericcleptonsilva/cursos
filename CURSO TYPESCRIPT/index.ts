6f964e27c54bd83dd4f2c03cae3f30d4dfa33570
ericcleptonsilva <ericcleptonsilva@gmail.com>
2021-09-27 09:56:23 -0300

    Update como funciona as interfaces

--- CURSO TYPESCRIPT/index.ts
+++ CURSO TYPESCRIPT/index.ts
@@ -1,4 +1,31 @@
-function soma(a: number, b: number) {
+// types
+// interfaces
+
+interface IAnimal {
+    nome: string;
+    tipo: 'terrestre' | 'aquático';
+    fazerSom(alturaEmDecibeis: number): void;
+}
+
+interface Ifelinos extends IAnimal{
+    visaoNoturna: boolean;
+}
+const animal: IAnimal = {
+    nome: 'Elefante',
+    tipo: 'terrestre',
+    fazerSom: (alturaEmDecibeis) => ('${alturaEmDecibeis}dB')
+}
+animal.fazerSom(55);
+
+const felinos: Ifelinos = {
+    nome: 'Onça',
+    tipo: 'terrestre',
+    visaoNoturna: true,
+    fazerSom: (alturaEmDecibeis) => ('${alturaEmDecibeis}dB')
+}
+felinos.fazerSom(10);
+
+/*const  soma = (a: number, b: number) => {
     return a + b;
 }
-console.log(soma(5, 0));
+console.log(soma(5, 0));*/
