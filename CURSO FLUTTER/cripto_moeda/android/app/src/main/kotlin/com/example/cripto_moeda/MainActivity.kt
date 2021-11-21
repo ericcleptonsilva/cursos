639db343c92129e0f51bdc1895a6d76c01f62207
ericcleptonsilva <ericcleptonsilva@gmail.com>
2021-11-21 02:51:53 -0300

    First Commit

--- CURSO FLUTTER/cripto_moeda/android/app/src/main/kotlin/com/example/cripto_moeda/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cripto_moeda
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- CURSO FLUTTER/cripto_moeda/lib/models/Posicao.dart
+++
@@ -1,7 +0,0 @@
-import 'package:cripto_moeda/models/Moedas.dart';
-
-class Posicao {
-  Moeda moeda;
-  double quantidade;
-  Posicao({required this.moeda, required this.quantidade});
-}
